2d4c9ed613dade6f3ad52ccc270260a748835845
Huang Jin <jimmy.huangjin@gmail.com>
2012-08-06 16:25:22 +0800

    rm useless files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,22 @@
-set :application, "zhue"
-set :repository,  "set your repository location here"
+require "bundler/capistrano"
+
+# If you have custom Sidekiq configuration options put them in config/sidekiq.yml
+# require "sidekiq/capistrano"
+
+set :application, "zhule"
+set :repository,  "nick@192.168.0.103:www/#{application}"
 
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-set :deploy_to, "/home/deploy/www/#{application}"
+set :deploy_to, "/home/nick/www/#{application}"
 set :branch, 'master'
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+server "192.168.0.103", :app, :web, :db, :primary => true
+set :user, 'deploy'
+set :use_sudo, false
+
+default_run_options[:pty] = true
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -19,10 +25,24 @@ role :db,  "your slave db-server here"
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+
+namespace :deploy do
+  task :start do
+    run "cd #{deploy_to}/current/; RAILS_ENV=production passenger start;"
+  end
+
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+task :mongoid_create_indexes, :roles => :web do
+  run "cd #{deploy_to}/current/; RAILS_ENV=production bundle exec rake db:mongoid:create_indexes"
+end
+
+task :compile_assets, :roles => :web do
+  run "cd #{deploy_to}/current/; RAILS_ENV=production bundle exec rake assets:precompile"
+end
+
+after 'deploy:update_code', :compile_assets
